0ddc3cf26bcc64fc7632799b3146db8d54c8bcd1
pimous <pimoos972@gmail.com>
2023-05-24 15:26:21 +0100

    ajout de craco

--- src/App/store/memes.js
+++ src/App/store/memes.js
@@ -2,64 +2,52 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import { saveCurrent } from './current';
 
 const initialState = {
-    images:[],
-    memes:[],
+    images: [],
+    memes: [],
 }
 
 const memes = createSlice({
-  name: 'listes',
-  initialState,
-  reducers: {
-    addImage:(state,action)=>{
-        state.images.push(action.payload);
+    name: 'listes',
+    initialState,
+    reducers: {
+        addImage: (state, action) => {
+            state.images.push(action.payload);
+        },
+        addImages: (state, action) => {
+            state.images.push(...action.payload);
+        }
     },
-    addImages:(state,action)=>{
-        state.images.push(...action.payload);
-    }
-  },
-  extraReducers:(builder)=>{
-    builder.addCase(
-        saveCurrent.fulfilled,(state,action)=>{
-            const pos = state.memes.findIndex(meme=>meme.id === action.payload.id)
-            if (pos >=0){
+    extraReducers: (builder) => {
+        builder.addCase('current/save', (state, action) => {
+            const pos = state.memes.findIndex(meme => meme.id === action.payload.id)
+            if (pos >= 0) {
                 //existant dans la liste
+
                 state.memes[pos] = action.payload;
             }
-            else
-            {
-                //absent de la liste
+            else {
+                //pas existant dans la liste
                 state.memes.push(action.payload);
             }
-        }
-    )
-    
-    builder.addCase(
-        fetchAllListesValues.fulfilled,
-        (state,action)=>{
+        })
+        builder.addCase(fetchAllListesValues.fulfilled, (state, action) => {
             state.images.push(...action.payload.images);
             state.memes.push(...action.payload.memes);
-        }
-    );
-
-    builder.addDefaultCase((state,action)=>{console.log(state,action)});
-
-  }
-
-});
-
-export const fetchAllListesValues = createAsyncThunk(
-    'listes/fetchAll',
-    async()=>{
-        const promiseImg = await fetch(`http://localhost:5689/images`);             
-        const promiseMeme = await fetch(`http://localhost:5689/memes`);     
-        const promiseALL = await Promise.all([promiseImg,promiseMeme]);
-        const imgs = await promiseALL[0].json();
-        const memes = await promiseALL[1].json();        
-
-        return {images:imgs,memes:memes};    
+            console.log(state, action)
+        })
+        builder.addDefaultCase((state, action) => { console.log(state, action) })
     }
-);
+});
+export const fetchAllListesValues = createAsyncThunk('listes/fetchAll', async () => {
 
-export const {addImage,addImages} = memes.actions
+    const prImg = await fetch(`http://localhost:5689/images`);
+    const prMeme = await fetch(`http://localhost:5689/memes`);
 
-export default memes.reducer
+    const prs = await Promise.all([prImg, prMeme]);
+    const imgs = await prs[0].json();
+    const memes = await prs[1].json();
+    return { images: imgs, memes: memes };
+})
+export const { addImage, addImages } = memes.actions;
+
+export default memes.reducer;
